f6329c4a458ed2995eaf333de70af6b4ce429c02
Ben <bengeraghty.bg@gmail.com>
2023-04-16 13:12:19 +0700

    add navigation bar with open and close function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('test');
+const hamMenu = document.querySelector('.hamburger-menu');
+const navigation = document.querySelector('.nav__list-container');
+
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    navigation.classList.toggle('show');
+    overlay.classList.toggle('show');
+    rightContent.classList.toggle('darken');
+
+    if (navigation.classList.contains('show')) {
+        navigation.classList.add('fade-in');
+        setTimeout(() => {
+            navigation.classList.remove('fade-in');
+        }, 500);
+    } else {
+        navigation.classList.add('fade-out');
+        setTimeout(() => {
+            navigation.classList.remove('fade-out');
+        }, 500);
+    }
+});
