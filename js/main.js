6e7794680933390a66f732b1b6ccf0737f3f5f95
Ben <bengeraghty.bg@gmail.com>
2023-04-16 14:20:08 +0700

    add header

--- js/main.js
+++ js/main.js
@@ -5,9 +5,7 @@ const navigation = document.querySelector('.nav__list-container');
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     navigation.classList.toggle('show');
-    overlay.classList.toggle('show');
-    rightContent.classList.toggle('darken');
-
+    
     if (navigation.classList.contains('show')) {
         navigation.classList.add('fade-in');
         setTimeout(() => {
